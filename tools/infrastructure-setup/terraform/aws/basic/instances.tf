769a79cbcbb6daffc615b84025f45de60b8689dd
andrew <4484551-atecey@users.noreply.gitlab.com>
2019-12-11 09:29:05 +0000

    Added test cluster

--- tools/infrastructure-setup/terraform/aws/basic/instances.tf
+++ tools/infrastructure-setup/terraform/aws/basic/instances.tf
@@ -6,15 +6,19 @@ provider "aws" {
 resource "aws_instance" "elk-stack" {
     count           = "${var.instance_count}"
     ami             = "${var.amazon_linux_2}"
-    instance_type   = "c5d.4xlarge"
+    instance_type   = "m5a.2xlarge"
     security_groups = ["${aws_security_group.setup.name}", "${aws_security_group.vpc_internal.name}"]
     key_name        = "${var.key}"
-    # ebs_block_device {
-    #     device_name = "/dev/xvda"
-    #     volume_type = "io1"
-    #     iops        = 16000
-    #     volume_size = 500
-    # }
+    ebs_block_device {
+        device_name = "/dev/xvda"
+        volume_type = "gp2"
+        volume_size = 20
+    }
+    ebs_block_device {
+        device_name = "/dev/sdb"
+        volume_type = "st1"
+        volume_size = 503
+    }
 }
 
 output "public_dns" {
---
+++ tools/infrastructure-setup/terraform/aws/demo_cluster/security-groups.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "setup" {
+  name        = "demo_cluster_setup"
+  description = "Default security group that allows inbound and outbound ssh traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
+  }
+}
+
+resource "aws_security_group" "vpc_internal" {
+  name        = "demo_cluster_vpc_internal"
+  description = "Default VPC security group"
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
